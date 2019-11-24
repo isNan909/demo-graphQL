2fa02609ffb84fc81b37a9bbc9166c7db798f554
Ishan Manandhar <ishan02016@gmail.com>
2019-11-24 09:24:22 +0545

    added working schema

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const schema = require('./schema/schema')
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
 
 app.listen(8080, () => {
     console.log('Server running succefully...')
---
+++ schema/schema.js
@@ -0,0 +1,63 @@
+const graphql = require('graphql');
+const L = require('lodash');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLBoolean
+} = graphql;
+
+var dish = [{
+        name: 'Lamb Salad with Fregola',
+        country: 'USA',
+        id: '1'
+    },
+    {
+        name: 'Belly pork & pineapple burritos',
+        country: 'Mexico',
+        id: '2'
+    },
+    {
+        name: 'Dumpling',
+        country: 'China',
+        id: '3'
+    }
+]
+
+const DishType = new GraphQLObjectType({
+    name: 'Dish',
+    fields: () => ({
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        tasty: {
+            type: GraphQLBoolean
+        }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        dish: {
+            type: DishType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parent, args) {
+                L.find(dish, {
+                    id: args.id
+                });
+            }
+        }
+    }
+})
+
+module.export = new GraphQLSchema({
+    query: RootQuery
+})
