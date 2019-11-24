8aab36ea8d2809668583a24326690aa47bd30248
Ishan Manandhar <ishan02016@gmail.com>
2019-11-24 13:30:05 +0545

    added mongo connection

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongo = require('mongoose');
 
 const app = express();
+
+mongo.connect('mongodb://admin:admin123##@ds053317.mlab.com:53317/gql-demo', {
+    useNewUrlParser: true
+})
+mongo.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
